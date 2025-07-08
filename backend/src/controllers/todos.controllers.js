53128889da09233c6dcc91360ece25877a4f847e
zeeshan26code <zeeshan26code@gmail.com>
2025-07-08 17:22:44 +0530

    update-todo feature created sucessfully && i have also added try-catch block which i forget to do

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -1,43 +1,90 @@
-const Todo = require('../models/todo.models.js');
+const Todo = require("../models/todo.models.js");
 
 const createTodo = async (req, res) => {
+  try {
     const { title, description } = req.body;
     const userId = req.userInfo.id;
 
     const newTodo = Todo.create({
-        title,
-        description,
-        status: 'pending',
-        createdBy: userId
+      title,
+      description,
+      status: "pending",
+      createdBy: userId,
     });
     return res.status(200).json({
-        success: true,
-        message: "Todo created successfully",
-        data: newTodo
+      success: true,
+      message: "Todo created successfully",
+      data: newTodo,
     });
-}
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
 
 const getTodos = async (req, res) => {
+  try {
     const userId = req.userInfo.id;
     const allTodosByLoggedInUser = await Todo.find({ createdBy: userId });
     console.log(allTodosByLoggedInUser);
     return res.status(200).json({
-        success: true,
-        message: "Todos fetched successfully",
-        data: allTodosByLoggedInUser
+      success: true,
+      message: "Todos fetched successfully",
+      data: allTodosByLoggedInUser,
     });
-}
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
 
 const updateTodo = async (req, res) => {
+  try {
     const { id } = req.params;
-    const { title, description } = req.body;
-    const updatedTodo = await Todo.findByIdAndUpdate(id, { title, description }, { new: true });
+    const { title, description, status } = req.body;
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      id,
+      { title, description, status: status || "pending" },
+      { new: true }
+    );
     return res.status(200).json({
-        success: true,
-        message: "Todo updated successfully",
-        data: updatedTodo
+      success: true,
+      message: "Todo updated successfully",
+      data: updatedTodo,
     });
-}
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
 
+const deleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await Todo.findByIdAndDelete(id);
+    if (deleteTodo) {
+      return res.status(200).json({
+        success: true,
+        message: "Todo deleted successfully",
+        data: deleteTodo,
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
 
-module.exports = { createTodo, getTodos, updateTodo };
+module.exports = { createTodo, getTodos, updateTodo, deleteTodo };
--- backend/src/routes/home.routes.js
+++ backend/src/routes/home.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createTodo,getTodos, updateTodo } = require('../controllers/todos.controllers.js');
+const { createTodo,getTodos, updateTodo, deleteTodo } = require('../controllers/todos.controllers.js');
 const authMiddleware = require('../middlewares/auth.middlewares.js');
 
 
 router.post('/create-todo', authMiddleware, createTodo);
 router.get('/get-todos', authMiddleware, getTodos);
 router.put('/update-todo/:id', authMiddleware, updateTodo);
+router.delete('/delete-todo/:id', authMiddleware, deleteTodo);
 
 module.exports = router;
